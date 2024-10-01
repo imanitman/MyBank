0c078d688829ab8dbc9d9b6cf960a5f489525385
imanitman <amn1022004@gmail.com>
2024-10-01 11:26:52 +0700

    authentication provider and profile in spring security

---
+++ src/main/java/com/example/secureApplication/config/CustomUsernamePasswordProdAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.example.secureApplication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("prod")
+@RequiredArgsConstructor
+public class CustomUsernamePasswordProdAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    /**
+     * @param authentication the authentication request object.
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String userPassword = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(userPassword, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, userPassword, userDetails.getAuthorities());
+        }
+        throw new BadCredentialsException("Invalid credentials");
+    }
+
+    /**
+     * @param authentication
+     * @return
+     */
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
--- src/main/java/com/example/secureApplication/config/ProjectSecurityProdConfiguration.java
+++ src/main/java/com/example/secureApplication/config/ProjectSecurityProdConfiguration.java
@@ -1,37 +1,32 @@
 package com.example.secureApplication.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-import org.springframework.security.authentication.password.CompromisedPasswordDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
+@Profile("prod")
 @Configuration
-@Profile("!prod")
-public class ProjectSecurityConfiguration {
+@RequiredArgsConstructor
+public class ProjectSecurityProdConfiguration {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.
-                csrf(csrfConfig -> csrfConfig.disable())
-                .authorizeHttpRequests((request)->
-                request.requestMatchers("/balances","/cards","/loans", "/accounts").authenticated()
-                            .requestMatchers("/notices","/contacts","/register").permitAll()
-        );
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/balances","/cards","/loans", "/accounts").authenticated()
+                        .requestMatchers("/notices","/contacts","/register").permitAll()
+                );
         httpSecurity.formLogin(Customizer.withDefaults());
         httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
