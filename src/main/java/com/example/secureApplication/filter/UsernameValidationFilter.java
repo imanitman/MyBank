d801c7c2a865db2ee7cf1e978587a11ae0658358
imanitman <amn1022004@gmail.com>
2024-10-10 22:21:57 +0700

    fix custom filter

--- src/main/java/com/example/secureApplication/filter/UsernameValidationFilter.java
+++ src/main/java/com/example/secureApplication/filter/UsernameValidationFilter.java
@@ -27,24 +27,29 @@ public class UsernameValidationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        String header = req.getHeader(HttpHeaders.AUTHORIZATION) ;
-        if (header != null){
+        String header = req.getHeader(HttpHeaders.AUTHORIZATION);
+        if(null != header) {
             header = header.trim();
-            byte[] decoded;
-            if (StringUtils.startsWithIgnoreCase(header, "Basic ")){
+            if(StringUtils.startsWithIgnoreCase(header, "Basic ")) {
                 byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
-                decoded = Base64.getDecoder().decode(base64Token);
-                String token = new String(decoded,StandardCharsets.UTF_8);
-                int delim = token.indexOf(':');
-                if (delim == -1){throw new BadCredentialsException("Your request is invalid");}
-                else{
-                    String username = token.substring(0,delim);
-                    if (username.toLowerCase().contains("test")){
-                        throw new BadCredentialsException("Your request is invalid");
+                byte[] decoded;
+                try {
+                    decoded = Base64.getDecoder().decode(base64Token);
+                    String token = new String(decoded, StandardCharsets.UTF_8); // un:pwd
+                    int delim = token.indexOf(":");
+                    if(delim== -1) {
+                        throw new BadCredentialsException("Invalid basic authentication token");
                     }
+                    String email = token.substring(0,delim);
+                    if(email.toLowerCase().contains("test")) {
+                        res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        return;
+                    }
+                } catch (IllegalArgumentException exception) {
+                    throw new BadCredentialsException("Failed to decode basic authentication token");
                 }
             }
         }
-        chain.doFilter(request,response);
+        chain.doFilter(request, response);
     }
 }
